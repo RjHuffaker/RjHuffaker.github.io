9220b702ae8700379b74ec280722cfedec501232
RjH <freedomsworn@gmail.com>
2018-02-05 14:09:16 -0700

    Incremented

--- scorpinator.js
+++ scorpinator.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Scorpinator
 // @namespace    http://RjHuffaker.github.io
-// @version      1.011
+// @version      1.012
 // @updateURL    http://RjHuffaker.github.io/scorpinator.js
 // @description  Provides various helper functions to PestPac, customized to our particular use-case.
 // @author       You
